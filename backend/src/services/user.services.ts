f0af2fbc5aefd7de58b115dead28904f6de53b12
nikshrma <nikshrma2006@gmail.com>
2025-12-05 13:26:30 +0530

    added user services

---
+++ backend/src/services/user.services.ts
@@ -0,0 +1,32 @@
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../generated/prisma/client.js"
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+import bcrypt from "bcrypt";
+import type { SigninInput, SignupInput } from "nikhlshrmadev-common-app"
+const saltRounds = 8;
+
+export async function checkUserExistance(email : string){
+    return await prisma.user.findUnique({
+        where:{email}
+    });
+}
+export async function createUser(userPayload: SignupInput){
+    const userHash = await bcrypt.hash(userPayload.password , saltRounds)
+    const user = await prisma.user.create({
+        data:{
+        email:userPayload.username,
+        password:userHash,
+        name:userPayload.name??null
+        //smth new i learned
+    }})
+    return user;
+}
+export async function signInUser(userPayload: SigninInput  , hash: string){
+    const a:boolean = await bcrypt.compare(userPayload.password , hash);
+    return a;
+}
