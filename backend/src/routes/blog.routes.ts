126834280502bdf3904b1598e28e2837a560fc21
nikshrma <nikshrma2006@gmail.com>
2025-12-05 13:26:52 +0530

    set up all endpoints

--- backend/src/routes/blog.routes.ts
+++ backend/src/routes/blog.routes.ts
@@ -1,3 +1,21 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 
 export const blogRouter = Router();
+
+/* POST /api/v1/blog
+PUT /api/v1/blog
+GET /api/v1/blog/:id
+GET /api/v1/blog/bulk */
+
+blogRouter.post("/" , (req:Request , res:Response)=>{
+
+})
+blogRouter.put("/" , (req:Request , res:Response)=>{
+    
+})
+blogRouter.get("/:id" , (req:Request , res:Response)=>{
+    
+})
+blogRouter.get("/bulk" , (req:Request , res:Response)=>{
+    
+})
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,3 +1,14 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+import jwt from 'jsonwebtoken';
+import type { SignupInput } from "nikhlshrmadev-common-app";
 
 export const userRouter = Router();
+
+userRouter.post('/signup' , (req : Request , res : Response)=>{
+    const userPayload: SignupInput = req.body;
+
+})
+
+userRouter.post('/signin' , (req : Request , res : Response)=>{
+    
+})
